7365d673e31d692c43d7939cc94f7718439535ac
Mechalopa <92443696+Mechalopa@users.noreply.github.com>
2024-06-02 14:51:55 +0900

    Partial backport

--- src/main/java/com/github/mechalopa/hmag/world/entity/HornetEntity.java
+++ src/main/java/com/github/mechalopa/hmag/world/entity/HornetEntity.java
@@ -36,6 +36,8 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.ServerLevelAccessor;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 
 public class HornetEntity extends AbstractFlyingMonsterEntity
 {
@@ -143,6 +145,12 @@ public class HornetEntity extends AbstractFlyingMonsterEntity
 	protected float getStandingEyeHeight(Pose pose, EntityDimensions size)
 	{
 		return 1.52F;
+	}
+
+	@OnlyIn(Dist.CLIENT)
+	public boolean isAngrySkin()
+	{
+		return this.isAlive() && (this.isAggressive() || this.getAttackPhase() != 0);
 	}
 
 	@Override
